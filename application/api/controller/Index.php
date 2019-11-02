f48316504737f57e45e44c5292d9e0a99b8fe25c
momo <xirizhifeng@163.com>
2019-11-02 13:54:00 +0800

    bug fixed

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -14,28 +14,32 @@ class Index extends Controller
 {
     public function index()
     {
-        //获取banner数据
-        $banner = db('articles')
-            ->field('id,article_title,article_img')
-            ->where('is_logic_del', 0)
-            ->where('is_top', 1)
-            ->select();
-        //获取最新的十条微语
-        $wish = db('gossips')
-            ->field('id,data_msg')
-            ->order('id desc')
-            ->limit(10)
-            ->select();
+        $page = input('page', 1);
+        if ($page == 1) {
+            //获取banner数据
+            $banner = db('articles')
+                ->field('id,article_title,article_img')
+                ->where('is_logic_del', 0)
+                ->where('is_top', 1)
+                ->select();
+            //获取最新的十条微语
+            $wish = db('gossips')
+                ->field('id,data_msg')
+                ->order('id desc')
+                ->limit(10)
+                ->select();
+        }
+
         //获取十条文章信息
         $article = db('articles')
             ->field('id,article_title,article_img,update_time,read_num')
             ->where('is_logic_del', 0)
             ->order('id desc')
-            ->limit(10)
+            ->page($page, 10)
             ->select();
         for ($i = 0; $i < count($article); $i++) {
             $article[$i]['update_time'] = date('Y-m-d H:i', $article[$i]['update_time']);
         }
-        return json(['banner' => $banner, 'wish' => $wish, 'artlist' => $article]);
+        return json(['banner' => $banner, 'wish' => $wish, 'artlist' => $article, 'page' => $page]);
     }
 }

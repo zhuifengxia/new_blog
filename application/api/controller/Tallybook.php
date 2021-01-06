23a6863d90dddc9039dfa0bf80be9334f17a882c
momo <xirizhifeng@163.com>
2021-01-06 15:25:37 +0800

    bug fixed

--- application/api/controller/Tallybook.php
+++ application/api/controller/Tallybook.php
@@ -165,7 +165,7 @@ class Tallybook extends Controller
                 //查看当前用户记录多少天了，获得第一笔的时间
                 $first_date = $baseModel->dataValue($this->dbconfig, "details", "create_time", "user_id={$member['id']}", "id asc");
                 if ($first_date) {
-                    $member["create_day"] = ceil((strtotime(date("Y-m-d")) - strtotime($first_date)) / 86400);
+                    $member["create_day"] = ceil((strtotime(date("Y-m-d")) - $first_date) / 86400);
                 } else {
                     $member["create_day"] = 0;
                 }

59afff90a0d98df0ca162618d45882ad0f859f53
momo <xirizhifeng@163.com>
2023-02-13 10:28:48 +0800

    bug fixed

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
     //网站配置
     'web_config'=>[
         'web_url'           => 'https://liuxiaomo.cn',//TODO 网站域名上线后需更换正式
-        'money_sum'         =>2304.81,
+        'money_sum'         =>2093.93,
         'gjj_money_sum'         =>785
     ],
 ];
